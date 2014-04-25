5de5338cd48935db83a0b3d200390d80828308e1
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-04-25 11:16:00 -0300

    Módulo Agenda

---
+++ Paradigma Funcional/Agenda.hs
@@ -0,0 +1,13 @@
+module Agenda (Agenda, Nome, Telefone, pesquisaNome, quantosContatos, insereNovoContato, retiraContato, mostraAgenda) where
+
+data Agenda a = [(Nome,Telefone)]
+
+pesquisaNome :: Agenda -> Nome -> Telefone
+
+quantosContatos :: Agenda -> Nome -> Int
+
+insereNovoContato :: Agenda -> Nome -> Telefone -> Agenda
+
+retiraContato :: Agenda -> Nome -> Telefone -> Agenda
+
+mostraAgenda :: Agenda -> String
