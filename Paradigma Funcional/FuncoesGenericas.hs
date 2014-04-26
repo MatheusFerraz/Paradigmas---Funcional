ff6e7653c037c3b7e2dbc936c46e5bfeadb2b5d1
Augusto Modesto <augusto.modesto@live.com>
2014-04-26 20:37:50 -0300

    Adicionado novo funcao de calcular quantidade de elementros em uma lista e adicionados cabecalhos a todas as funcoes

--- Paradigma Funcional/FuncoesGenericas.hs
+++ Paradigma Funcional/FuncoesGenericas.hs
@@ -1,10 +1,12 @@
-module FuncoesGenericas (somaLista, multiplicaLista, subtraiValores) where
+module FuncoesGenericas (somaLista, multiplicaLista, subtraiValores, contaLista) where
 
 --Funcao para Soma dos Valores da Lista utilizando recursividade
+somaLista :: [Float] -> Float
 somaLista [] = 0
 somaLista (h:t) = h + (somaLista t)
 
 --Funcao para multiplicacao dos Valores da Lista utilizando funcao pronta do Haskell (product)
+multiplicaLista :: [Float] -> Float
 multiplicaLista (h:t) = product((h:t))
 
 --Funcao para realizar subtracao entre dois numeros fornecidos
@@ -13,3 +15,7 @@ subtraiValores n1 n2 = if(n1>n2)
 					   then (n1-n2)
 					   else (n2-n1)
 
+--Funcao para realizar a contagem de elemento em uma lista
+contaLista :: [Float] -> Float
+contaLista [] = 0
+contaLista (h:t) = 1 + (contaLista t)
