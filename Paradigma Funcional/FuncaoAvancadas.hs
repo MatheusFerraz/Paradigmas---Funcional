eda594db33ad74fe8605711b3ef7f496373e6b92
Augusto Modesto <augusto.modesto@live.com>
2014-04-26 19:20:03 -0300

    Adicionado a funcao de calculo de exponencial com numeros reais

--- Paradigma Funcional/FuncaoAvancadas.hs
+++ Paradigma Funcional/FuncaoAvancadas.hs
@@ -1,6 +1,11 @@
-module FuncoesAvancadas (fatorial) where
+module FuncoesAvancadas (fatorial, exponencial) where
 
 --Funcao para calcular um numero fatorial utilizando recursividade
 fatorial :: Int -> Int
 fatorial 0 = 1
 fatorial numero = numero * fatorial (numero-1)
+
+--Funcao para calcular um exponencial de numero reais utilizando recursividade
+exponencial :: Float -> Float -> Float
+exponencial base 0 = 1
+exponencial base expoente = base * exponencial base (expoente-1)
