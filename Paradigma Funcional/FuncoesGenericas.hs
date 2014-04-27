f687dd70ae31d2bff504ae35fe4b7862dadbb56e
MatheusFerraz <matheus_ccmf@yahoo.com.br>
2014-04-27 01:15:02 -0300

    Novas funções acrescentadas ao Módulo Funcoes Genericas

--- Paradigma Funcional/FuncoesGenericas.hs
+++ Paradigma Funcional/FuncoesGenericas.hs
@@ -1,4 +1,4 @@
-module FuncoesGenericas (somaLista, multiplicaLista, subtraiValores, contaLista) where
+module FuncoesGenericas (somaLista, multiplicaLista, subtraiValores, contaLista, calculaMdc, calculaDelta, calculaSolQuadratica) where
 
 --Funcao para Soma dos Valores da Lista utilizando recursividade
 somaLista :: [Float] -> Float
@@ -15,7 +15,22 @@ subtraiValores n1 n2 = if(n1>n2)
 					   then (n1-n2)
 					   else (n2-n1)
 
---Funcao para realizar a contagem de elemento em uma lista
+--Funcao para realizar a contagem de elementos em uma lista
 contaLista :: [Float] -> Float
 contaLista [] = 0
 contaLista (h:t) = 1 + (contaLista t)
+
+--Funcao para realizar calculo do MDC com dois numeros de entrada utilizando funcao pronta do Haskell (mdc)
+calculaMdc :: Int -> Int -> Int
+calculaMdc n1 n2 | n1 < n2 = calculaMdc n2 n1
+                 | n2 == 0 = n1
+                 | otherwise = calculaMdc n2 (mod n1 n2)
+
+--Funcao para calcular o delta de uma funcao de segundo grau
+calculaDelta :: Float -> Float -> Float -> Float
+calculaDelta va vb vc = ((vb^2) - (4 * va * vc))
+
+--Funcao para calcular a solucao de uma equacao de segundo grau
+calculaSolQuadratica :: Float -> Float -> Float -> (Float, Float)
+calculaSolQuadratica va vb vc = if(calculaDelta va vb vc) < 0.0  then undefined
+	                            else ((((-vb) + sqrt(calculaDelta va vb vc)) / (2.0 * va)), (((-vb) - sqrt(calculaDelta va vb vc)) / (2.0 * va)))
